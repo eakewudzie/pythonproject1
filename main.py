51474144cde60a9474a57b9364b4428fd44b8637
Eunice <eunice.ewudzie@amalitech.org>
2024-10-18 11:43:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello")
+
+status="hungry"
+print(type(status))
+
+# variable container for a value
+
+first_name = "Eunice"
+last_name = "Ewudzie"
+full_name = first_name + " " + last_name
+print(full_name)
