3ca069b16f97306db4a91de0de97c0dc926386fa
Eunice <amakakra44@gmail.com>
2024-10-25 03:27:13 +0200

    third commit

--- python_file1.py
+++ python_file1.py
@@ -11,10 +11,18 @@ print(name.find("a"))
 
 
 #this is to check the next occurrence of a in a text with multiple aas
-name = "Eunice Kakraa"
+name = "Eunice Kakraa "
 first_a_index = name.find("a")
 second_a_index = name.find("a", first_a_index + 1)
 print(second_a_index)
-
+#useful methods in python
 third_a_index = name.find("a", second_a_index + 1)
 print(third_a_index)
+print(name.capitalize())  # if there is an additional space or string...only capitalizes the first word
+print(name.upper())
+print(name.lower())
+print(name.isdigit())  #if value we have is a digit or not , return false if it isn't
+print(name.isalpha())  #check to see if strings contains only alphabetical letters. it returns false if it has as a space
+print(name.count("a"))
+print(name.replace("a","i")) #replace an alphabet with a different one
+print(name*3)
