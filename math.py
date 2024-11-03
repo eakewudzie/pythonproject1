cae75b6b04ba98a280fe731709807a43ede84d8e
Eunice <amakakra44@gmail.com>
2024-11-03 19:37:41 +0100

    new line

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+
+pi = 3.14
+sec_pi=-3.14
+
+print(round(pi))
+print(math.ceil(pi))#round a number up
+print(abs(pi))# get the absolute number of a figure
+print(math.floor(pi))# round down number
+# print(abs(sec_pi))
