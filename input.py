a30e213c46c7c043b527ab9f21ddded0ee941624
Eunice <amakakra44@gmail.com>
2024-11-06 22:11:31 +0100

    new lines of code added to input file

--- input.py
+++ input.py
@@ -1,3 +1,7 @@
 #input("What is your name?:")
+#input accepts value from user and display...making use of type casting here
 name = input("What is your name?:")
+age = int(input("What is your age?:"))
+
 print("Hello "+name)
+print("You are " +str(age) + "years old")
