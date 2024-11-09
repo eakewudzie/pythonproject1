7c3e69c3f3d644b20296d32155611f0e2265fbec
Eunice <amakakra44@gmail.com>
2024-11-09 05:18:39 +0100

    new file if_statements added

---
+++ if_statements.py
@@ -0,0 +1,8 @@
+#if statement: check if condition is true then execute
+
+name = str(input("Enter your Name: "))
+age =int(input("Enter your age: "))
+
+
+if age >= 18:
+    print("Great, You are a grown up " + name)
