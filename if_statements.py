d9f5a50991aabfe5c9ed9c14e6a87835d69260ad
Eunice <amakakra44@gmail.com>
2024-11-09 05:27:34 +0100

    modified recent file

--- if_statements.py
+++ if_statements.py
@@ -4,5 +4,9 @@ name = str(input("Enter your Name: "))
 age =int(input("Enter your age: "))
 
 
-if age >= 18:
+if age >= 27:
     print("Great, You are a grown up " + name)
+elif age == 26:
+    print("you are the exact age we need. Continue with your application")
+else:
+    print("You are below our expected age and hence not eligible")
