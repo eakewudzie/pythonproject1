2417ba384e1be25bc198a445d2cb2169ae5b198c
Eunice <amakakra44@gmail.com>
2024-11-13 22:25:37 +0100

    for_loop file added

---
+++ for _loop.py
@@ -0,0 +1,17 @@
+# for loops execute block of code for a limited amount of time
+
+
+
+# for i in range(10):#last number is exclusive
+#     print(i)
+
+
+
+# for i in range(45,89):
+#     print(i)
+# that is the number from 45 to 88
+
+#if I want to include  89 then i do this
+
+for i in range(45,89):
+    print(i+1)
