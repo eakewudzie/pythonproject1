19053f413afc8913099556a0adc2a67a158f9efb
Eunice <amakakra44@gmail.com>
2024-11-22 21:13:17 +0100

    changes made in list.py file

--- list.py
+++ list.py
@@ -1,1 +1,16 @@
- 
+# list = used to store multiple items in a single variable
+
+food =["pizza", "chicken", "fries"]
+
+# print(food)
+# print(food[0])
+
+food[0]="jollof"
+
+
+# print(food)
+
+for y in food:
+    print(y)
+
+
