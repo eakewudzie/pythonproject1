7b8d8f8404e587211aa832e51b56796f9ba0c5bd
Eunice <amakakra44@gmail.com>
2024-11-23 15:43:41 +0100

    updatd list.py file

--- list.py
+++ list.py
@@ -12,6 +12,12 @@ food[0]="jollof"
 food.append("ice cream")
 food.append("grilled chicken")
 
+# food.remove("jollof")
+food.pop()
+food.insert(0,"cake")
+food.sort()
+# food.clear() # this clears all enries in the list
+
 # for loop to print each item in the list
 for y in food:
     print(y)
