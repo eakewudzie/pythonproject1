480cbd305b9f1bbab0dc59e5994ab7015a6fe1ed
Eunice <amakakra44@gmail.com>
2024-11-23 15:53:30 +0100

    new file added

---
+++ 2D_lists_ListofLists.py
@@ -0,0 +1,11 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee","soda","coke"]
+dinner = ["pizza","hamburger","jollof"]
+desert = ["toffee","biscuit","chocolate"]
+breakfast = ["oat","tea"]
+
+food = [breakfast,drinks,dinner,desert]
+print(food)
+print(food[0])   #gives the first part of the list food which in this case is breakfast
+print(food[0][0])   #gives he first part of the list of the firs list which is oat in breakfast
