d0041dc2f9491b043ee8e4e7d3d595e6be291654
Eunice <amakakra44@gmail.com>
2024-11-24 22:31:35 +0100

    set.py file added

---
+++ set.py
@@ -0,0 +1,18 @@
+#set= colleection which is unordered , unindexed    and doesn't allow uplicates
+
+# Unordered: Sets do not maintain the order of their elements. The items in a set may appear in a different order each time you access them.
+
+# Unindexed: Sets do not support indexing or slicing because they are unordered.
+
+# No Duplicates: Sets automatically remove duplicate values. If you add duplicates, they will be stored only once.
+
+utensils = {"fork","spoon","knife","saucepan"}
+
+
+for x in utensils:
+    print(x)
+
+
+utensils.add("cutting board")
+# utensils.remove("knife")
+print(utensils)
