ebc311ad50525565b455196012a6062961b4c4ab
Eunice <amakakra44@gmail.com>
2024-12-01 22:05:05 +0100

    new file functions.py added

---
+++ functions.py
@@ -0,0 +1,19 @@
+#function = block of code which is executed only when it is called
+# functions only execute their block of code only when called
+
+# Parameter: A variable in the function definition that acts as a placeholder for input values.
+# Argument: The actual value passed to the function when it is called, assigned to the parameter.
+
+
+def hello():
+    print("Hello, it is nice to meet you!")
+
+
+hello()
+
+
+def greetings(name):
+    print("Hello, " +name)
+
+
+greetings("Eunice")
