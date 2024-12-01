9eb95c3495bca9f7b420074d2e5d1518416d3227
Eunice <amakakra44@gmail.com>
2024-12-01 21:33:12 +0100

    updated

--- index_operator.py
+++ index_operator.py
@@ -8,9 +8,12 @@ if (name[0].islower()):
 print(name)
 
 
-name1 = "Eunice Panyin"
+name1 = "Eunice Panyin!"
 
-first_name =name1[:6].lower()
+first_name =name1[:6].lower() #string slicing ...excludes last index of sring
 last_name = name1[7:].upper()
+last_charact = name1[-1] # last item referenced this way
 
 print(first_name)
+print(last_name)
+print(last_charact)
