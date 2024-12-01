d03ed2897f7eb012d8d608a82416793a7c05b9c6
Eunice <amakakra44@gmail.com>
2024-12-01 21:21:01 +0100

    file updated

--- index_operator.py
+++ index_operator.py
@@ -1,8 +1,16 @@
 # index operator[] = gives access to a sequence's eleent(str,list,tuples)
 
-name = "Eunice Kakra"
+name = "eunice Kakra"
 
 if (name[0].islower()):
     name = name.capitalize()
 
 print(name)
+
+
+name1 = "Eunice Panyin"
+
+first_name =name1[:6].lower()
+last_name = name1[7:].upper()
+
+print(first_name)
