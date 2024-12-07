5c4aa5d51e7d4b370379833aa3fd122665de6608
Eunice <amakakra44@gmail.com>
2024-12-07 23:15:49 +0100

    updated

---
+++ keyword_arguments.py
@@ -0,0 +1,8 @@
+# keyword arguments = argumens preceded by an idenifier when we pass them to a function 
+# The order of the arguments doesn't matter, unlike positional arguments Python knows the names of the arguments that our function receives
+
+def Hello(first,middle,last):
+    print("Hello " +first+" "+middle+"  "+last)
+
+
+Hello("Eunice","Kakra","Ama")
