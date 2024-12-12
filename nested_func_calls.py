5889f38e258fcf98b1b91dd398a1607146905623
Eunice <amakakra44@gmail.com>
2024-12-12 16:24:03 +0100

    modified

--- nested_func_calls.py
+++ nested_func_calls.py
@@ -1,3 +1,9 @@
+# nested functions calls = function calls inside other function calls
+                        #    innermost function calls are resolved first
+                        #    returned value is used as argument for the nex outer function
+
+
+
 num = input("Enter a whole positive number: ")
 
 
@@ -5,3 +11,8 @@ num = float(num)
 num = abs(num)
 num = round(num)
 print(num)
+
+# the above can be done using nested func calls
+
+
+print(round(abs(float(input("Enter any value ")))))
