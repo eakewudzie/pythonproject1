7899ba2641f04bfbe38e0abf0de2c58e7f1d9d62
Eunice <amakakra44@gmail.com>
2024-12-12 16:55:15 +0100

    updated

---
+++ scope_of_Var.py
@@ -0,0 +1,16 @@
+# scope = the region that a variable is recognized. 
+#          A variable is only available inside the region it is created.
+#         A global and locally scoped versions of a variable can be created
+
+
+# name = "Eunice"
+
+
+def display_name(nme):
+    # name = "Ewudzie"
+    print(nme)
+# display_name()
+
+display_name("E")
+
+# print(name)
