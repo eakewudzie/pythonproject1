d83155d276e598b07123844b93e89defc5d31548
Eunice <amakakra44@gmail.com>
2024-12-12 17:00:24 +0100

    modified

--- scope_of_Var.py
+++ scope_of_Var.py
@@ -3,14 +3,36 @@
 #         A global and locally scoped versions of a variable can be created
 
 
-# name = "Eunice"
+name = "Eunice"
 
 
-def display_name(nme):
-    # name = "Ewudzie"
-    print(nme)
-# display_name()
+def display_name():
+    name = "Ewudzie"
+    print(name)
 
-display_name("E")
+display_name() #gives the local variable name
 
-# print(name)
+
+# this is for the global variable
+print(name)
+
+
+
+ #-------------DECLARING AND USING A GLOBAL VARIABLE IN A  FUNC------------------
+ #-----------to modify a global variable inside a function, you must explicitly declare it 
+ # using the global keyword. Without global, Python will treat any assignment to the variable as 
+ # creating a new local variable.-------
+
+ 
+x = 10  # Global variable
+
+def my_function():
+    global x  # Declare x as global
+    x = x + 5  # Modify global x
+    print(x)
+
+my_function()
+# Output: 15
+
+print(x)  # Global variable is now modified
+# Output: 15
