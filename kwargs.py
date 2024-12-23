636f0be407f2255c1452dd1e0b8552b4015408e4
Eunice <amakakra44@gmail.com>
2024-12-23 23:55:07 +0100

    updated

--- kwargs.py
+++ kwargs.py
@@ -1,3 +1,5 @@
 # **kwargs =  parameter that will pack all arguments into a dictionary
 #             useful so that a function can accept a carying amoun of keyword arguments
 
+def Hello(**kwargs):
+    print("Hello")
