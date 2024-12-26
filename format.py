0c81eb9c26cd0f1f92a9e99a4a1e7cf0155ee0cc
Eunice <amakakra44@gmail.com>
2024-12-26 18:26:27 +0100

    updated

--- format.py
+++ format.py
@@ -2,7 +2,13 @@
 # str.format() = opional method that gives users
 #               more control when displying output
 
-animal = "cat"
-item = "moon"
+# animal = "cat"
+# item = "moon"
 
-print("the " +animal+" jumped over the " +item)
+# print("the " +animal+" jumped over the " +item)
+# writing the above code using the format method
+
+print("the {} jumped over the {}".format("cat","moon"))
+
+
+# ------NB----------format method is always called on a string, so it must come after the closing quotation mark of the string it is formatting. This is because format is a method of the str class in Python,
