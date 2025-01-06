2d843ded5a9b1a6701abb37286ea1cf814a2f010
Eunice <amakakra44@gmail.com>
2025-01-06 19:14:20 +0100

    updated

--- My_random.py
+++ My_random.py
@@ -5,6 +5,15 @@ import random
 
 y = random.randint(1,10)
 z = random.random() #generates a random floating-point number between 0.0 (inclusive) and 1.0 (exclusive)
+newList = ['apple','paper']
+m = random.choice(newList)
 
 print(y)
 print(z)
+# print(random.choice(newList))
+print(m)
+
+
+cards = [1,2,3,4,5,6,"M","N","O"]
+random.shuffle(cards)
+print(cards)
