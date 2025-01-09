4aa6042f1e0917a02df0eec409848423aa8f3008
Eunice <amakakra44@gmail.com>
2025-01-09 18:02:23 +0100

    updated

--- My_exception.py
+++ My_exception.py
@@ -1,8 +1,18 @@
 # exception = events detected during execution that interrupt the flow of a program
 
-numerator = int(input("Enter a number to divide"))
-denominator = int(input("Enter a number to divide by:"))
+# numerator = int(input("Enter a number to divide: "))
+# denominator = int(input("Enter a number to divide by: "))
 
-result = numerator / denominator
+# result = numerator / denominator
 
-print(result)
+# print(result)
+# for instance , division by 0 would throw an error. To prevent this, we introduce try block
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+
+    result = numerator / denominator
+
+    print(result)
+except ZeroDivisionError:
+    print("You cannot divide a number by Zero: You should know this")
