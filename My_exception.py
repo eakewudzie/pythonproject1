441cf4dee5b3bc144cc4bc7d0f86db857bcf76ce
Eunice <amakakra44@gmail.com>
2025-01-09 17:53:33 +0100

    updated

---
+++ My_exception.py
@@ -0,0 +1,8 @@
+# exception = events detected during execution that interrupt the flow of a program
+
+numerator = int(input("Enter a number to divide"))
+denominator = int(input("Enter a number to divide by:"))
+
+result = numerator / denominator
+
+print(result)
