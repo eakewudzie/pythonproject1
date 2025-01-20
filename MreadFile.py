b90a164328280658098c99f5c7b9b161d1ce1037
Eunice <amakakra44@gmail.com>
2025-01-20 20:59:07 +0100

    updated

--- MreadFile.py
+++ MreadFile.py
@@ -1,4 +1,6 @@
 with open('file.txt') as file:
     print(file.read())
 
-print(file.closed)
+# print(file.closed) #to check the file if it has closed. Hence this code return true if closed and false if otherwise
+#  the use of with --------It ensures the file is properly closed, even if
+#  an exception occurs within the block.
