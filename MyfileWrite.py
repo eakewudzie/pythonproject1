ca9c63e6914fbf1d79731b330c3fae49c6ac0ae5
Eunice <amakakra44@gmail.com>
2025-01-22 21:15:10 +0100

    updated

--- MyfileWrite.py
+++ MyfileWrite.py
@@ -1,3 +1,9 @@
-text = " Hi there"
+# writing a file. After running the program... it creates the file with the name test.txt in our project folder
+
+text = " Hi there\nThis is Eunice. Great to meet you!"
 with open('test.txt','w') as file:
     file.write(text)
+
+
+# to append a text to it, all you have to do is type the statements or texts in the variable text and
+#  then replace w with a which stand for append
