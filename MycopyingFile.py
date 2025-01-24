77c7c2086ab7cec1c4b16f1d435faa4bf02970f0
Eunice <amakakra44@gmail.com>
2025-01-24 21:47:07 +0100

    updated

--- MycopyingFile.py
+++ MycopyingFile.py
@@ -3,7 +3,7 @@ import shutil
 # he shutil model
 # copyfile() = copies contents of a file
 # copy() =     copyfile() + permission mode + desination can be a directory
-# copy2() =    copy() + copies metadata(file's creation and modification times)
+# copy2() =    copy() +..... copies metadata(file's creation and modification times)
 
 
 shutil.copyfile('test.txt','copiedFile.txt') #source,destination
