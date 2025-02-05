1f3013f0131ff4a76f88079e5c18599420bdecd4
Eunice <amakakra44@gmail.com>
2025-02-05 12:56:26 +0100

    updated

--- MyFiledelete.py
+++ MyFiledelete.py
@@ -1,8 +1,19 @@
 import os
 path = "text.txt"
+pathDir = "AnemptyFolder"
 # os.remove('path')
 
+# try:
+#     os.remove('path')
+# except FileNotFoundError:
+#     print("The File not found")
+
+
 try:
-    os.remove('path')
+    os.rmdir(pathDir)
 except FileNotFoundError:
-    print("File not found here")
+    print("That file was not found")
+except PermissionError:
+    print("You don't have he persmission to delete this")
+else:
+    print("we canno find the file")
