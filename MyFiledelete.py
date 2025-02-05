0a42c6ad96f4350bfea585ac0483334f84b6519b
Eunice <amakakra44@gmail.com>
2025-02-05 13:08:03 +0100

    updated

--- MyFiledelete.py
+++ MyFiledelete.py
@@ -1,19 +1,24 @@
+# Removed the duplicate else block – The else should 
+# only be used once after try-except.
+
 import os
 path = "text.txt"
 pathDir = "AnemptyFolder"
 # os.remove('path')
 
-# try:
-#     os.remove('path')
-# except FileNotFoundError:
-#     print("The File not found")
+try:
+    os.remove('path')
+except FileNotFoundError:
+    print("The File not found")
 
 
 try:
-    os.rmdir(pathDir)
+    os.rmdir(pathDir) # this only allows folders which are empty to be moved
 except FileNotFoundError:
     print("That file was not found")
 except PermissionError:
     print("You don't have he persmission to delete this")
+except:
+    print("the directory is not empty and hence cannot be removed")
 else:
-    print("we canno find the file")
+    print(pathDir + " was deleted")
