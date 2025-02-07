e957c63983d9eda8253ffab8483c1211096239d0
Eunice <amakakra44@gmail.com>
2025-02-07 02:30:48 +0100

    created my module and used in a different file

---
+++ myModule.py
@@ -0,0 +1,8 @@
+# module = a file containing python code. May contain functions, classes,....etc
+# used wih modular programming, which is to separate a program into parts
+import myMsg
+
+myMsg.hello()
+
+
+# print("hi")
---
+++ myMsg.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello, Hope you are having a great day")
+
+
+def bye():
+    print("Goodbye for now. Have a great day")
