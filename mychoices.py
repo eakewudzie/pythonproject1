95fa9b568c97ab46bde45e24dab3fc054c923525
Eunice <amakakra44@gmail.com>
2025-02-12 17:49:33 +0100

    updated

--- mychoices.py
+++ mychoices.py
@@ -1,22 +1,3 @@
-# import random
-# rock paper scissors game
-# choices = ["rock","paper","scissors"]
-
-# computer = random.choice(choices)
-# player = input("rock,paper, or scissors?: ")
-
-
-
-# print("computer: ", computer)
-# print("player: " ,player) 
-
-
-
-
-
-
-
-
 
 # rock ,paper , scissors game
 
