c51b9008b4e3e4297352b49c33dfb9b2504d4c8a
Eunice <amakakra44@gmail.com>
2025-02-12 17:47:34 +0100

    updated

--- mychoices.py
+++ mychoices.py
@@ -1,11 +1,39 @@
-import random
+# import random
+# rock paper scissors game
+# choices = ["rock","paper","scissors"]
 
-choices = ["rock","paper","scissors","bag"]
+# computer = random.choice(choices)
+# player = input("rock,paper, or scissors?: ")
+
+
+
+# print("computer: ", computer)
+# print("player: " ,player) 
+
+
+
+
+
+
+
+
+
+# rock ,paper , scissors game
+
+import random
+choices = ["rock","scissors","paper"]
 
 computer = random.choice(choices)
-player = input("rock,paper, or scissors?: ").lower()
 
-print(player)
+person1 = input("Enter your choice: rock,paper or scissors: " )
+if person1 not in choices:
+    print("select from the choices here")
+else:
+    print(person1)
+
 print(computer)
+# print(person1)
+
+
 
 
