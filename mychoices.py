1ee35495613726e049f5403a567f1b14ed64d1bd
Eunice <amakakra44@gmail.com>
2025-02-13 21:05:02 +0100

    updated

--- mychoices.py
+++ mychoices.py
@@ -11,14 +11,20 @@ while person1 not in choices:
     person1 = input("Enter your choice: rock,paper or scissors: " )
     
 
+if person1 == computer:
+    print("computer: ", computer)
+    print("person1: ", person1)
+    print("Tie!")
+elif person1 == "rock":
+    if computer == "paper":
+        print("computer: ", computer)
+        print("person1: ", person1)
+        print("You lose!")
 
 
-# if 
 
-
-
-print("computer: ",  computer)
-print(person1)
+# print("computer: ",  computer)
+# print(person1)
 
 
 
