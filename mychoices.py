f3a8888d7592982ea1e735efed3f639b48f4c822
Eunice <amakakra44@gmail.com>
2025-02-14 19:44:49 +0100

    updated

--- mychoices.py
+++ mychoices.py
@@ -1,31 +1,31 @@
-
 # rock ,paper , scissors game
 # print("Enter one of the choices provided: ")
 import random
-choices = ["rock","scissors","paper"]
+while True:
+    choices = ["rock","scissors","paper"]
 
-computer = random.choice(choices)
-person1 = None
+    computer = random.choice(choices)
+    person1 = None
 
-while person1 not in choices:
-    person1 = input("Enter your choice: rock,paper or scissors: " )
+    while person1 not in choices:
+        person1 = input("Enter your choice: rock,paper or scissors: " )
     
 
-if person1 == computer:
-    print("computer: ", computer)
-    print("person1: ", person1)
-    print("Tie!")
-elif person1 == "rock":
-    if computer == "paper":
+    if person1 == computer:
         print("computer: ", computer)
         print("person1: ", person1)
+        print("Tie!")
+    elif person1 == "rock":
+        if computer == "paper":
+         print("computer: ", computer)
+         print("person1: ", person1)
         print("You lose!")
-    if computer == "scissors":
-        print("computer: ", computer)
-        print("person1: ", person1)
-        print("You win")
+        if computer == "scissors":
+         print("computer: ", computer)
+         print("person1: ", person1)
+         print("You win")
 
-        
+
 
 
 
