ecf94832af5a29690f8b52be8a9c220a9c2c6e7b
Eunice <amakakra44@gmail.com>
2025-03-06 14:14:49 +0100

    updated

--- newGame.py
+++ newGame.py
@@ -16,10 +16,15 @@ questions ={
     "Who created Python?: " "A",
     "What year was Python creaed?: ": "B",
     "Python is tributed to which comedy group?" :"C",
-    "Is the Earth round? ": "A"
+    "Is the Earth round? ": "D"
 
 }
 
-options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates"]
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+          ["A. 1989", "B. 1991", "C. 2000", "D. 2010" ],
+          ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+          ["A. True", "B. False", "C. Maybe", "D. Don't know"]
     
 ]
+
+new_game() 
